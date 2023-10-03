4736733bd8b32869324ae3e9f96c63bc87510b48
c0a2207193 <C0A2207193@edu.teu.ac.jp>
2023-10-03 14:04:31 +0900

    一回目

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,6 @@
 import sys
 import pygame as pg
-
+import random
 
 WIDTH, HEIGHT = 1600, 900
 
@@ -11,6 +11,16 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+
+    # 爆弾のSurfaceを作成
+    bomb_surface = pg.Surface((20, 20), pg.SRCALPHA)
+    pg.draw.circle(bomb_surface, (255, 0, 0), (10, 10), 10)
+
+    # 爆弾のRectの位置をランダムに設定
+    bomb_rect = bomb_surface.get_rect()
+    bomb_rect.x = random.randint(0, WIDTH - bomb_rect.width)
+    bomb_rect.y = random.randint(0, HEIGHT - bomb_rect.height)
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -20,6 +30,10 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+
+         # 爆弾を画面に描画
+        screen.blit(bomb_surface, bomb_rect.topleft)
+        
         pg.display.update()
         tmr += 1
         clock.tick(10)
